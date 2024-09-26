6438cf7eabf46f83f526a0d5be51ce24bb3a8c7a
Thinh-Tran-0902 <tranthinh5801@gmail.com>
2024-09-26 13:40:36 +0700

    add new models product, mode get, create, update, delete

---
+++ api/models/product.js
@@ -0,0 +1,58 @@
+const db = require('../config/db'); 
+
+// Get product by ID
+const getProductById = (id, callback) => {
+    const query = `SELECT * FROM Product WHERE id = ?;`;
+    db.query(query, [id], (error, results) => {
+        if (error) {
+            return callback(error, null);
+        }
+        if (results.length > 0) {
+            return callback(null, results[0]); 
+        } else {
+            return callback(null, null); 
+        }
+    });
+};
+
+// Create product
+const createProduct = (name, description, price, quantity, callback) => {
+    const query = `
+        INSERT INTO Product (name, description, price, quantity)
+        VALUES (?, ?, ?, ?);`;
+    db.query(query, [name, description, price, quantity], (error, results) => {
+        if (error) {
+            return callback(error, null);
+        }
+        return callback(null, results.affectedRows); 
+    });
+};
+
+// Update product by ID
+const updateProductById = (id, updateProductData, callback) => {
+    const query = `UPDATE Product SET ? WHERE id = ?;`;
+    db.query(query, [updateProductData, id], (error, results) => {
+        if (error) {
+            return callback(error, null);
+        }
+        return callback(null, results.affectedRows); 
+    });
+};
+
+// Delete product by ID
+const deleteProductById = (id, callback) => {
+    const query = `DELETE Product WHERE id = ?;`;
+    db.query(query, [id], (error, results) => {
+        if (error) {
+            return callback(error, null);
+        }
+        return callback(null, results.affectedRows); 
+    });
+};
+
+module.exports = {
+    getProductById,
+    createProduct,
+    updateProductById,
+    deleteProductById
+};
