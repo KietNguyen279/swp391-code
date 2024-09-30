401050a4562342550f80ede6e190d3c7bd1b958f
Thinh-Tran-0902 <tranthinh5801@gmail.com>
2024-09-30 12:47:26 +0700

    add function create and delete for waterValue model

--- api/models/waterValue.js
+++ api/models/waterValue.js
@@ -1,6 +1,6 @@
 const db = require('../config/db');
 
-// Get water param value by water param id and name of param
+// Get water param value by id of water and name of value
 const getWaterParamByIdAndName = (id, name, callback) => {
     const query = `SELECT param_value FROM Water_parameter_value WHERE water_parameters_id = ? AND name = '?';`;
     db.query(query, [id, name], (error, results) => {
@@ -15,10 +15,34 @@ const getWaterParamByIdAndName = (id, name, callback) => {
     });
 };
 
-// Update water param value by id and name of value
+// Create water param value
+const createWaterParam = (name, param_value, water_parameters_id, callback) => {
+    const query = `
+        INSERT INTO Product (name, param_value, water_parameters_id)
+        VALUES ('?', ?, ?);`;
+    db.query(query, [name, param_value, water_parameters_id], (error, results) => {
+        if (error) {
+            return callback(error, null);
+        }
+        return callback(null, results.affectedRows); 
+    });
+};
+
+// Update water param value by id of water and name of value
 const updateWaterParamByIdAndName = (id, name, updateValue, callback) => {
     const query = `UPDATE Water_parameter_value SET param_value = ? WHERE water_parameters_id = ? AND name = '?';`;
     db.query(query, [updateValue, id, name], (error, results) => {
+        if (error) {
+            return callback(error, null);
+        }
+        return callback(null, results.affectedRows); 
+    });
+};
+
+// Delete water param by id of water and name of value
+const deleteWaterParam = (id, name, callback) => {
+    const query = `DELETE Water_parameter_value WHERE water_parameters_id = ? AND name = '?';`;
+    db.query(query, [id, name], (error, results) => {
         if (error) {
             return callback(error, null);
         }
