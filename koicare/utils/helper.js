a51cb9ae4bcbfa269a2681b2c2689d0d481d6afc
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-25 02:14:28 +0700

    modified helper.js

--- koicare/utils/helper.js
+++ koicare/utils/helper.js
@@ -1,22 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 // Generate JWT token
-const generateToken = (userId) => {
-    return jwt.sign({ id: userId },'secret', { expiresIn: 60 * 60 });
+const generateToken = (userId, roleId) => { 
+  return jwt.sign({ id: userId, role_id: roleId }, process.env.JWT_SECRET, { expiresIn: '1h' });
 };
 
 // Verify JWT token
 const verifyToken = (bearerToken) => {
-    try {
-        const token = bearerToken.split(' ')[1];
-        const decoded = jwt.verify(token, 'secret');
-        return decoded; 
-    } catch (err) {
-        return null; 
-    }
+  try {
+    const token = bearerToken.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+    return decoded; 
+  } catch (err) {
+    return null; 
+  }
 };
 
 module.exports = {
-    generateToken,
-    verifyToken
+  generateToken,
+  verifyToken
 };
