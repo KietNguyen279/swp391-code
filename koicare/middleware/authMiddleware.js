c05c1f6b800c3f226615a145a39ecf27caae7e89
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-25 02:02:43 +0700

    modified authMiddleware

--- koicare/middleware/authMiddleware.js
+++ koicare/middleware/authMiddleware.js
@@ -2,25 +2,27 @@ const jwt = require('jsonwebtoken');
 
 // Middleware to verify JWT token
 const verifyTokenMiddleware = (req, res, next) => {
-    const token = req.headers.authorization; 
+  const token = req.headers.authorization;
 
-    if (!token) {
-        return res.status(401).json({ message: 'No token provided' });
-    }
+  if (!token) {
+    return res.status(401).json({
+      message: 'No token provided'
+    });
+  }
 
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);  
-
-      req.userId = decoded.id; 
-      req.userRole = decoded.role_id; 
-      next(); 
-    } catch (err) {
-      return res.status(401).json({ message: 'Failed to authenticate token' });
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    req.userRole = decoded.role_id;
+    next();
+  } catch (err) {
+    console.error("JWT Verification failed:", err);
+    return res.status(401).json({ message: 'Failed to authenticate token' });
+  }
 };
 
 // Middleware to verify JWT token and check role
-const verifyTokenAndRole = (allowedRoles) => { 
+const verifyTokenAndRole = (allowedRoles) => {
   return (req, res, next) => {
     const token = req.headers.authorization;
 
@@ -29,10 +31,10 @@ const verifyTokenAndRole = (allowedRoles) => {
     }
 
     try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);  
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.userId = decoded.id; 
-      req.userRole = decoded.role_id; 
+      req.userId = decoded.id;
+      req.userRole = decoded.role_id;
 
       if (allowedRoles.includes(req.userRole)) {
         next();
