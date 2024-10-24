424e9893d6183684b29882561980483693f9af88
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-25 01:38:27 +0700

    modified auth.js

--- koicare/routes/auth.js
+++ koicare/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const { generateToken } = require('../utils/helper');
+const { verifyToken } = require('../utils/helper');
 
 const User = require('../models/user');
 const { verifyToken: verifyTokenMiddleware, verifyTokenAndRole } = require('../middleware/authMiddleware');
@@ -31,7 +32,7 @@ router.post('/register', async (req, res) => {
             }
         });
     } catch (error) {
-        console.error('Error in registration:', error); 
+        console.error('Error in registration:', error);
         return res.status(500).json({ error: error.toString() });
     }
 });
@@ -47,33 +48,41 @@ router.post('/login', (req, res) => {
 
         User.getUserByEmail(email, async (error, user) => {
             if (error) {
-                console.log(error);
+                console.error('Error during login:', error);
                 return res.status(500).json({ error: error.toString() });
             }
-            if (user) {
-                const isMatch = await bcrypt.compare(password, user.password);
-                if (isMatch) {
-                    const token = generateToken(user.id);
-                    return res.json({
-                        message: "Login successful",
-                        token,
-                    });
-                } else {
-                    return res.status(401).json({ message: "Invalid credentials" });
-                }
+            if (!user) {
+                return res.status(401).json({ message: "User not found" });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (isMatch) {
+                const token = generateToken(user.id);
+                return res.json({
+                    message: "Login successful",
+                    token,
+                });
             } else {
-                return res.status(401).json({ message: "Invalid credentials" });
+                return res.status(401).json({ message: "Password doesn't match" });
             }
         });
     } catch (error) {
-        console.log(error);
+        console.error('Error in login:', error);
         return res.status(500).json({ error: error.toString() });
     }
 });
 
 // View Profile 
 router.get('/profile', verifyTokenMiddleware, (req, res) => {
-    const userId = req.userId;
+
+    const token = req.headers.authorization;
+    const decoded = verifyToken(token);
+
+    if (!decoded) {
+        return res.status(401).json({ message: 'Failed to authenticate token' });
+    }
+
+    const userId = decoded.id;
 
     User.getUserById(userId, (error, user) => {
         if (error) {
@@ -95,7 +104,15 @@ router.get('/profile', verifyTokenMiddleware, (req, res) => {
 
 // Update Profile  
 router.put('/profile/', verifyTokenMiddleware, (req, res) => {
-    const userId = req.userId;
+
+    const token = req.headers.authorization;
+    const decoded = verifyToken(token);
+
+    if (!decoded) {
+        return res.status(401).json({ message: 'Failed to authenticate token' });
+    }
+
+    const userId = decoded.id;
     const updatedUserData = req.body;
 
     if (updatedUserData.name && updatedUserData.name.length === 0) {
