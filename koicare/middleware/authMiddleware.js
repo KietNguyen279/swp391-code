b22c57001575d6690a8a5b0a18c3a7a07bc96df5
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-24 16:20:19 +0700

    fixed middleware

--- koicare/middleware/authMiddleware.js
+++ koicare/middleware/authMiddleware.js
@@ -1,5 +1,24 @@
 const jwt = require('jsonwebtoken');
 
+// Middleware to verify JWT token
+const verifyTokenMiddleware = (req, res, next) => {
+    const token = req.headers.authorization; 
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);  
+
+      req.userId = decoded.id; 
+      req.userRole = decoded.role_id; 
+      next(); 
+    } catch (err) {
+      return res.status(401).json({ message: 'Failed to authenticate token' });
+    }
+};
+
 // Middleware to verify JWT token and check role
 const verifyTokenAndRole = (allowedRoles) => { 
   return (req, res, next) => {
