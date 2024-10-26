8950e3323669a86dd2550f8cff63fee6444ecd58
KietNguyen279 <nkiet1319@gmail.com>
2024-10-27 03:17:57 +0700

    modified swagger & auth.js routes

--- koicare/routes/auth.js
+++ koicare/routes/auth.js
@@ -9,16 +9,16 @@ const { verifyTokenMiddleware } = require('../middleware/authMiddleware');
 // Register
 router.post('/register', async (req, res) => {
   try {
-    const { name, email, password, password_confirm, role_id } = req.body;
+    const { name, email, password, password_confirm, role } = req.body;
 
-    if (!name || !email || !password || !password_confirm || !role_id) {
+    if (!name || !email || !password || !password_confirm || !role) {
       return res.status(400).json({ message: "Please fill in all fields" });
     }
     if (password !== password_confirm) {
       return res.status(400).json({ message: "Passwords do not match" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    User.createUser({ name, email, password: hashedPassword, role_id }, (error, results) => {
+    User.createUser({ name, email, password: hashedPassword, role }, (error, results) => {
       if (error) {
         console.error('Error registering user:', error);
         if (error.code === 'ER_DUP_ENTRY') {
@@ -64,7 +64,7 @@ router.post('/login', async (req, res) => {
 
       const isMatch = await bcrypt.compare(password, user.password);
       if (isMatch) {
-        const token = generateToken(user.id, user.role_id);
+        const token = generateToken(user.id, user.role);
         return res.json({
           message: "Login successful",
           token,
