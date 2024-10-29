ad48051308a8dacbc096646b770c82377f511eb0
KietNguyen279 <nkiet1319@gmail.com>
2024-10-29 08:54:17 +0700

    add validation for product, role, user, waterParam, waterValue models

--- koicare/models/waterParam.js
+++ koicare/models/waterParam.js
@@ -13,6 +13,29 @@ const getAllWaterParameters = (callback) => {
 
 // Get water parameter by ID
 const getWaterParameterById = (id, callback) => {
+    // Input validation
+    if (isNaN(id) || id <= 0) {
+        return callback(new Error('Invalid water parameter ID'), null);
+    }
+    if (!Number.isInteger(id)) {
+        return callback(new Error('Invalid water parameter ID. Must be an integer.'), null);
+    }
+    if (id === '') {
+        return callback(new Error('Invalid water parameter ID. Must not be empty.'), null);
+    }
+    if (id === null) {
+        return callback(new Error('Invalid water parameter ID. Must not be null.'), null);
+    }
+    if (id === undefined) {
+        return callback(new Error('Invalid water parameter ID. Must not be undefined.'), null);
+    }
+    if (id === false) {
+        return callback(new Error('Invalid water parameter ID. Must not be false.'), null);
+    }
+    if (id === true) {
+        return callback(new Error('Invalid water parameter ID. Must not be true.'), null);
+    }
+
     const query = `SELECT * FROM Water_parameters WHERE id = ?;`;
     db.query(query, [id], (error, results) => {
         if (error) {
@@ -29,8 +52,61 @@ const getWaterParameterById = (id, callback) => {
 
 // Create water parameter
 const createWaterParameter = (measurement_time, pond_id, callback) => {
+    // Input validation
     if (!measurement_time || !pond_id) {
         return callback(new Error('Invalid input data. Please check all fields.'), null);
+    }
+    // Additional validation 
+    if (typeof measurement_time !== 'string') {
+        return callback(new Error('Invalid measurement_time. It must be a string.'), null);
+    }
+    if (isNaN(pond_id) || pond_id <= 0) {
+        return callback(new Error('Invalid pond_id.'), null);
+    }
+    if (!Number.isInteger(pond_id)) {
+        return callback(new Error('Invalid pond_id. Must be an integer.'), null);
+    }
+    if (pond_id === '') {
+        return callback(new Error('Invalid pond_id. Must not be empty.'), null);
+    }
+    if (pond_id === null) {
+        return callback(new Error('Invalid pond_id. Must not be null.'), null);
+    }
+    if (pond_id === undefined) {
+        return callback(new Error('Invalid pond_id. Must not be undefined.'), null);
+    }
+    if (pond_id === false) {
+        return callback(new Error('Invalid pond_id. Must not be false.'), null);
+    }
+    if (pond_id === true) {
+        return callback(new Error('Invalid pond_id. Must not be true.'), null);
+    }
+    if (measurement_time === '') {
+        return callback(new Error('Invalid measurement_time. Must not be empty.'), null);
+    }
+    if (measurement_time === null) {
+        return callback(new Error('Invalid measurement_time. Must not be null.'), null);
+    }
+    if (measurement_time === undefined) {
+        return callback(new Error('Invalid measurement_time. Must not be undefined.'), null);
+    }
+    if (measurement_time === false) {
+        return callback(new Error('Invalid measurement_time. Must not be false.'), null);
+    }
+    if (measurement_time === true) {
+        return callback(new Error('Invalid measurement_time. Must not be true.'), null);
+    }
+    if (measurement_time.length > 255) {
+        return callback(new Error('Invalid measurement_time. Must not exceed 255 characters.'), null);
+    }
+    if (measurement_time.length < 1) {
+        return callback(new Error('Invalid measurement_time. Must be at least 1 character.'), null);
+    }
+    if (pond_id.length > 255) {
+        return callback(new Error('Invalid pond_id. Must not exceed 255 characters.'), null);
+    }
+    if (pond_id.length < 1) {
+        return callback(new Error('Invalid pond_id. Must be at least 1 character.'), null);
     }
 
     const query = `
@@ -46,8 +122,51 @@ const createWaterParameter = (measurement_time, pond_id, callback) => {
 
 // Update water parameter by ID
 const updateWaterParameterById = (id, measurement_time, callback) => {
-    if (!measurement_time) {
-        return callback(new Error('Invalid input data. Measurement time is required.'), null);
+    // Input validation
+    if (isNaN(id) || id <= 0) {
+        return callback(new Error('Invalid water parameter ID'), null);
+    }
+    if (!measurement_time || typeof measurement_time !== 'string') {
+        return callback(new Error('Invalid input data. Measurement time is required and must be a string.'), null);
+    }
+    if (measurement_time === '') {
+        return callback(new Error('Invalid measurement_time. Must not be empty.'), null);
+    }
+    if (measurement_time === null) {
+        return callback(new Error('Invalid measurement_time. Must not be null.'), null);
+    }
+    if (measurement_time === undefined) {
+        return callback(new Error('Invalid measurement_time. Must not be undefined.'), null);
+    }
+    if (measurement_time === false) {
+        return callback(new Error('Invalid measurement_time. Must not be false.'), null);
+    }
+    if (measurement_time === true) {
+        return callback(new Error('Invalid measurement_time. Must not be true.'), null);
+    }
+    if (measurement_time.length > 255) {
+        return callback(new Error('Invalid measurement_time. Must not exceed 255 characters.'), null);
+    }
+    if (measurement_time.length < 1) {
+        return callback(new Error('Invalid measurement_time. Must be at least 1 character.'), null);
+    }
+    if (!Number.isInteger(id)) {
+        return callback(new Error('Invalid water parameter ID. Must be an integer.'), null);
+    }
+    if (id === '') {
+        return callback(new Error('Invalid water parameter ID. Must not be empty.'), null);
+    }
+    if (id === null) {
+        return callback(new Error('Invalid water parameter ID. Must not be null.'), null);
+    }
+    if (id === undefined) {
+        return callback(new Error('Invalid water parameter ID. Must not be undefined.'), null);
+    }
+    if (id === false) {
+        return callback(new Error('Invalid water parameter ID. Must not be false.'), null);
+    }
+    if (id === true) {
+        return callback(new Error('Invalid water parameter ID. Must not be true.'), null);
     }
 
     const query = `UPDATE Water_parameters SET measurement_time = ? WHERE id = ?;`;
@@ -61,6 +180,29 @@ const updateWaterParameterById = (id, measurement_time, callback) => {
 
 // Delete water parameter by ID
 const deleteWaterParameterById = (id, callback) => {
+    // Input validation
+    if (isNaN(id) || id <= 0) {
+        return callback(new Error('Invalid water parameter ID'), null);
+    }
+    if (!Number.isInteger(id)) {
+        return callback(new Error('Invalid water parameter ID. Must be an integer.'), null);
+    }
+    if (id === '') {
+        return callback(new Error('Invalid water parameter ID. Must not be empty.'), null);
+    }
+    if (id === null) {
+        return callback(new Error('Invalid water parameter ID. Must not be null.'), null);
+    }
+    if (id === undefined) {
+        return callback(new Error('Invalid water parameter ID. Must not be undefined.'), null);
+    }
+    if (id === false) {
+        return callback(new Error('Invalid water parameter ID. Must not be false.'), null);
+    }
+    if (id === true) {
+        return callback(new Error('Invalid water parameter ID. Must not be true.'), null);
+    }
+    
     const query = `DELETE FROM Water_parameters WHERE id = ?;`;
     db.query(query, [id], (error, results) => {
         if (error) {
--- koicare/models/waterValue.js
+++ koicare/models/waterValue.js
@@ -2,6 +2,47 @@ const db = require('../config/db');
 
 // Get water param value by id of water and name of value
 const getWaterParamByIdAndName = (id, name, callback) => {
+    // Input validation
+    if (isNaN(id) || id <= 0) {
+        return callback(new Error('Invalid water parameter ID'), null);
+    }
+    if (typeof name !== 'string' || name.length === 0) {
+        return callback(new Error('Invalid water parameter name'), null);
+    }
+    if (!Number.isInteger(id)) {
+        return callback(new Error('Invalid water parameter ID. Must be an integer.'), null);
+    }
+    if (id === '') {
+        return callback(new Error('Invalid water parameter ID. Must not be empty.'), null);
+    }
+    if (id === null) {
+        return callback(new Error('Invalid water parameter ID. Must not be null.'), null);
+    }
+    if (id === undefined) {
+        return callback(new Error('Invalid water parameter ID. Must not be undefined.'), null);
+    }
+    if (id === false) {
+        return callback(new Error('Invalid water parameter ID. Must not be false.'), null);
+    }
+    if (id === true) {
+        return callback(new Error('Invalid water parameter ID. Must not be true.'), null);
+    }
+    if (name === '') {
+        return callback(new Error('Invalid water parameter name. Must not be empty.'), null);
+    }
+    if (name === null) {
+        return callback(new Error('Invalid water parameter name. Must not be null.'), null);
+    }
+    if (name === undefined) {
+        return callback(new Error('Invalid water parameter name. Must not be undefined.'), null);
+    }
+    if (name === false) {
+        return callback(new Error('Invalid water parameter name. Must not be false.'), null);
+    }
+    if (name === true) {
+        return callback(new Error('Invalid water parameter name. Must not be true.'), null);
+    }
+
     const query = `SELECT param_value FROM Water_parameter_value WHERE water_parameters_id = ? AND name = ?;`;
     db.query(query, [id, name], (error, results) => {
         if (error) {
@@ -18,10 +59,54 @@ const getWaterParamByIdAndName = (id, name, callback) => {
 
 // Create water param value
 const createWaterParam = (name, param_value, water_parameters_id, callback) => {
+    // Input validation
     if (!name || !param_value || !water_parameters_id) {
         return callback(new Error('Invalid input data. Please check all fields.'), null);
     }
-    
+    // Additional validation 
+    if (typeof name !== 'string' || name.length === 0) {
+        return callback(new Error('Invalid name'), null);
+    }
+    if (isNaN(param_value)) {
+        return callback(new Error('Invalid param_value'), null);
+    }
+    if (isNaN(water_parameters_id) || water_parameters_id <= 0) {
+        return callback(new Error('Invalid water_parameters_id'), null);
+    }
+    if (!Number.isInteger(water_parameters_id)) {
+        return callback(new Error('Invalid water_parameters_id. Must be an integer.'), null);
+    }
+    if (water_parameters_id === '') {
+        return callback(new Error('Invalid water_parameters_id. Must not be empty.'), null);
+    }
+    if (water_parameters_id === null) {
+        return callback(new Error('Invalid water_parameters_id. Must not be null.'), null);
+    }
+    if (water_parameters_id === undefined) {
+        return callback(new Error('Invalid water_parameters_id. Must not be undefined.'), null);
+    }
+    if (water_parameters_id === false) {
+        return callback(new Error('Invalid water_parameters_id. Must not be false.'), null);
+    }
+    if (water_parameters_id === true) {
+        return callback(new Error('Invalid water_parameters_id. Must not be true.'), null);
+    }
+    if (name === '') {
+        return callback(new Error('Invalid name. Must not be empty.'), null);
+    }
+    if (name === null) {
+        return callback(new Error('Invalid name. Must not be null.'), null);
+    }
+    if (name === undefined) {
+        return callback(new Error('Invalid name. Must not be undefined.'), null);
+    }
+    if (name === false) {
+        return callback(new Error('Invalid name. Must not be false.'), null);
+    }
+    if (name === true) {
+        return callback(new Error('Invalid name. Must not be true.'), null);
+    }
+
     const query = `
     INSERT INTO Water_parameter_value (name, param_value, water_parameters_id)  
     VALUES (?, ?, ?);`;
@@ -35,8 +120,48 @@ const createWaterParam = (name, param_value, water_parameters_id, callback) => {
 
 // Update water param value by id of water and name of value
 const updateWaterParamByIdAndName = (id, name, updateValue, callback) => {
-    if (!updateValue) {
-        return callback(new Error('Invalid input data. Param value is required.'), null);
+    // Input validation
+    if (isNaN(id) || id <= 0) {
+        return callback(new Error('Invalid water parameter ID'), null);
+    }
+    if (typeof name !== 'string' || name.length === 0) {
+        return callback(new Error('Invalid water parameter name'), null);
+    }
+    if (updateValue === undefined || updateValue === null || isNaN(updateValue)) { // More specific check for updateValue
+        return callback(new Error('Invalid input data. Param value is required and must be a number.'), null);
+    }
+    if (!Number.isInteger(id)) {
+        return callback(new Error('Invalid water parameter ID. Must be an integer.'), null);
+    }
+    if (id === '') {
+        return callback(new Error('Invalid water parameter ID. Must not be empty.'), null);
+    }
+    if (id === null) {
+        return callback(new Error('Invalid water parameter ID. Must not be null.'), null);
+    }
+    if (id === undefined) {
+        return callback(new Error('Invalid water parameter ID. Must not be undefined.'), null);
+    }
+    if (id === false) {
+        return callback(new Error('Invalid water parameter ID. Must not be false.'), null);
+    }
+    if (id === true) {
+        return callback(new Error('Invalid water parameter ID. Must not be true.'), null);
+    }
+    if (name === '') {
+        return callback(new Error('Invalid water parameter name. Must not be empty.'), null);
+    }
+    if (name === null) {
+        return callback(new Error('Invalid water parameter name. Must not be null.'), null);
+    }
+    if (name === undefined) {
+        return callback(new Error('Invalid water parameter name. Must not be undefined.'), null);
+    }
+    if (name === false) {
+        return callback(new Error('Invalid water parameter name. Must not be false.'), null);
+    }
+    if (name === true) {
+        return callback(new Error('Invalid water parameter name. Must not be true.'), null);
     }
 
     const query = `UPDATE Water_parameter_value SET param_value = ? WHERE water_parameters_id = ? AND name = ?;`;
@@ -50,6 +175,47 @@ const updateWaterParamByIdAndName = (id, name, updateValue, callback) => {
 
 // Delete water param by id
 const deleteWaterParam = (id, name, callback) => {
+    // Input validation
+    if (isNaN(id) || id <= 0) {
+        return callback(new Error('Invalid water parameter ID'), null);
+    }
+    if (typeof name !== 'string' || name.length === 0) {
+        return callback(new Error('Invalid water parameter name'), null);
+    }
+    if (!Number.isInteger(id)) {
+        return callback(new Error('Invalid water parameter ID. Must be an integer.'), null);
+    }
+    if (id === '') {
+        return callback(new Error('Invalid water parameter ID. Must not be empty.'), null);
+    }
+    if (id === null) {
+        return callback(new Error('Invalid water parameter ID. Must not be null.'), null);
+    }
+    if (id === undefined) {
+        return callback(new Error('Invalid water parameter ID. Must not be undefined.'), null);
+    }
+    if (id === false) {
+        return callback(new Error('Invalid water parameter ID. Must not be false.'), null);
+    }
+    if (id === true) {
+        return callback(new Error('Invalid water parameter ID. Must not be true.'), null);
+    }
+    if (name === '') {
+        return callback(new Error('Invalid water parameter name. Must not be empty.'), null);
+    }
+    if (name === null) {
+        return callback(new Error('Invalid water parameter name. Must not be null.'), null);
+    }
+    if (name === undefined) {
+        return callback(new Error('Invalid water parameter name. Must not be undefined.'), null);
+    }
+    if (name === false) {
+        return callback(new Error('Invalid water parameter name. Must not be false.'), null);
+    }
+    if (name === true) {
+        return callback(new Error('Invalid water parameter name. Must not be true.'), null);
+    }
+
     const query = `DELETE FROM Water_parameter_value WHERE water_parameters_id = ? AND name = ?;`;
     db.query(query, [id, name], (error, results) => {
         if (error) {
